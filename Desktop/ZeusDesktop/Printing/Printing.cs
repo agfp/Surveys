c1d23cc3e9c0fd05f5b3e640e381fc773e94aaec
alexandre giuseppe <alexandregiuseppe@gmail.com>
2014-07-03 02:37:35 -0300

    Printing in two columns, each question in a table row

--- Desktop/ZeusDesktop/Printing/Printing.cs
+++ Desktop/ZeusDesktop/Printing/Printing.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Windows.Documents;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ZeusDesktop
 {
@@ -35,24 +36,25 @@ namespace ZeusDesktop
 
             Table table = new Table();
             fd.Blocks.Add(table);
-            table.Columns.Add(new TableColumn() { Width = new GridLength(25) });
+            //table.Columns.Add(new TableColumn() { Width = new GridLength(25) });
             table.Columns.Add(new TableColumn());
 
             TableRowGroup rowgroup = new TableRowGroup();
             table.RowGroups.Add(rowgroup);
 
+            for (int j = 0; j < 2; j++)
             for (int i = 0; i < questions.Count; i++)
             {
                 TableRow row = new TableRow();
 
-                TableCell cell1 = new TableCell();
-                row.Cells.Add(cell1);
+                //TableCell cell1 = new TableCell();
+                //row.Cells.Add(cell1);
 
-                Paragraph number = new Paragraph() { FontWeight = FontWeights.Bold };
-                cell1.Blocks.Add(number);
-                number.Inlines.Add((i + 1) + ".");
+                //Paragraph number = new Paragraph() { FontWeight = FontWeights.Bold };
+                //cell1.Blocks.Add(number);
+                //number.Inlines.Add((i + 1) + ".");
 
-                var cell2 = NewMethod(questions[i]);
+                var cell2 = NewMethod(questions[i], i+1);
                 row.Cells.Add(cell2);
 
                 rowgroup.Rows.Add(row);
@@ -60,7 +62,7 @@ namespace ZeusDesktop
                 TableRow r = new TableRow();
                 rowgroup.Rows.Add(r);
 
-                TableCell c = new TableCell() { ColumnSpan = 2 };
+                TableCell c = new TableCell() { ColumnSpan = 1 };
                 r.Cells.Add(c);
                 c.Blocks.Add(space);
             }
@@ -70,15 +72,17 @@ namespace ZeusDesktop
             fd.PageHeight = pd.PrintableAreaHeight;
             fd.PageWidth = pd.PrintableAreaWidth;
             fd.PagePadding = new Thickness(50);
-            fd.ColumnGap = 0;
-            fd.ColumnWidth = pd.PrintableAreaWidth;
+            fd.ColumnGap = 20;
+            fd.ColumnWidth = pd.PrintableAreaWidth / 3;
+            fd.ColumnRuleBrush = new SolidColorBrush(Colors.Black);
+            fd.ColumnRuleWidth = 1;
 
             IDocumentPaginatorSource dps = fd;
             pd.PrintDocument(dps.DocumentPaginator, survey);
 
         }
 
-        private static TableCell NewMethod(Questions question)
+        private static TableCell NewMethod(Questions question, int i)
         {
             TableCell cell2 = new TableCell();
 
@@ -88,14 +92,34 @@ namespace ZeusDesktop
             cell2.Blocks.Add(q1);
             cell2.Blocks.Add(q2);
 
-            q1.Inlines.Add(new Bold(new Run(question.Question)));
-            q2.Inlines.Add("Instrução genérica");
+            q1.Inlines.Add(new Bold(new Run(i + ". " + question.Question)));
+            
+
+            switch (question.Type)
+            {
+                case 1:
+                    q2.Inlines.Add("Preencha o campo");
+                    break;
+
+                case 2:
+                    q2.Inlines.Add("Selecione até " + question.NumAnswers + " opções");
+                    break;
+
+                case 3:
+                case 4:
+                    q2.Inlines.Add("Selecione uma opção");
+                    break;
+
+            }
+
 
             if (!String.IsNullOrEmpty(question.Instruction))
             {
-                Paragraph q3 = new Paragraph();
-                cell2.Blocks.Add(q3);
-                q3.Inlines.Add(new Italic(new Run(question.Instruction)));
+                //Paragraph q3 = new Paragraph();
+                //cell2.Blocks.Add(q3);
+                q2.Inlines.Add(" (");
+                q2.Inlines.Add(new Italic(new Run(question.Instruction)));
+                q2.Inlines.Add(")");
             }
 
             switch (question.Type)
